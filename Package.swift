540545fe99f3e707efd14aa4938742d5efb73d26
Ruben <rcagnie@wayfair.com>
2019-09-23 08:42:56 -0400

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "Theo", targets: ["Theo"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Neo4j-Swift/Bolt-swift.git", .branch("master"))
+        .package(url: "https://github.com/Neo4j-Swift/Bolt-swift.git", from: "5.0.0")
         /*.package(url: "https://github.com/lukaskubanek/LoremSwiftum.git", .revision("6c6018aec1c61a40c2aa06a68eb843ce42bca262")),*/
     ],
     targets: [
